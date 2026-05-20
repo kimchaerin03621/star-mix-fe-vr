7be928a2d28655c2ab870a90bfc0c93d1dc7335b
kcr <kimchaerin03621@gmail.com>
2026-05-20 15:41:25 +0900

    feat: enable HTTPS development server using @vitejs/plugin-basic-ssl

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [react()],
+  plugins: [
+    react(),
+    basicSsl()
+  ],
   resolve: {
     dedupe: ['three']
+  },
+  server: {
+    host: true,
+    https: true
   }
 })
